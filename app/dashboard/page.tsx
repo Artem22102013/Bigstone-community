1f7a1f4290c2153e0d1d1a0d05c3ee4de66f1748
Artem22102013 <119873267+Artem22102013@users.noreply.github.com>
2025-07-13 22:01:30 +0400

    finished oauth2

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
-import { supabase } from '../../../lib/supabaseClient';
+import { supabase } from '../../lib/supabaseClient';
 import type { User } from '@supabase/supabase-js';
 
 export default function DashboardPage() {
